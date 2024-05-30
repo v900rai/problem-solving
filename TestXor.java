4e81bc9c0709d4a64a4f9b99a535009da6abba47
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-30 19:46:12 +0530

    Add files via upload

---
+++ TestXor.java
@@ -0,0 +1,15 @@
+package selfpractices;
+
+public class TestXor {
+    public static void main(String args[]){
+        int x=5;
+        int y=7;
+        //declaring values
+// bitwise XOR
+// 0101 ^ 0111 = 0101 = 2
+// Performing an operation with xor and traditional operator
+        System.out.println("x ^ y = " + (x ^ y));
+    }
+
+}
+
