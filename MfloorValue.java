cf16271a18126d2f49ac69556e5802e2d6009e9e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-30 19:46:49 +0530

    Add files via upload

---
+++ MfloorValue.java
@@ -0,0 +1,8 @@
+package selfpractices;
+
+public class MfloorValue {
+    public static void main(String args[]){
+        double n=Math.floor(5.9999d);
+        System.out.println(" floor value  "+n);
+    }
+}
