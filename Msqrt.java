1ed9341a6708993f7898286d6f54707ee070c6e9
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-30 19:47:18 +0530

    Add files via upload

---
+++ Msqrt.java
@@ -0,0 +1,11 @@
+package selfpractices;
+
+public class Msqrt {
+    public static void main(String args[]){
+        double n=Math.sqrt(625);
+        System.out.println("Squre root of 625 "+n);
+
+
+
+    }
+}
