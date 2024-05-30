b2915054e3f3a04d881de051c09674541829fedb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-30 19:46:23 +0530

    Add files via upload

---
+++ SimpleArrays.java
@@ -0,0 +1,10 @@
+package selfpractices;
+
+public class SimpleArrays {
+    public static void main(String []args){
+        int arr[]={ 10,20,30,40,50};
+        for(int i=0; i<5; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
