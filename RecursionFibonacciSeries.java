cedce4c57364b9a20f0a6595a61d72c33018e1e0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-06-12 10:59:34 +0530

    Add files via upload

---
+++ RecursionFibonacciSeries.java
@@ -0,0 +1,24 @@
+package RecursionAllProgram;
+
+public class RecursionFibonacciSeries {
+    //0,1,1,2,3,5,,8 this fibonacciSeries hai ye
+    public static void main(String[] args) {
+        int n=5;
+        if(n==0){
+            System.out.println(0);
+        }
+        else {
+            int [] fib=new int[n+1];
+          fib[0]=0;
+          fib[1]=1;
+            for (int i=2; i<=n; i++){
+                fib[i]=fib[i-1]+fib[i-2];
+            }
+            System.out.println("The Fibonacci Series up to "+n+"th term:");
+            for(int i=0; i<=n; i++){
+                System.out.println(fib[i]);
+            }
+        }
+
+    }
+}
